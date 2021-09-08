c6aa34ffa87e3112fbce8f153dbeb36c360175d4
Samuel Atobatele <swumatob@yahoo.co.uk>
2021-09-08 15:28:16 +0100

    Override devise registration controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'profile' },
+  controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
   root to: "pages#home"
 end
