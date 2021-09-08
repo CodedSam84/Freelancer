b6a491434cc5eb0d06052238cec43c0a9918b0aa
Samuel Atobatele <swumatob@yahoo.co.uk>
2021-09-08 22:48:26 +0100

    Created user dashboard page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,22 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+
+  def dashboard
+  end
+
+  def update
+    if current_user.update(current_user_params)
+      flash[:notice] = 'Record successfully updated!'
+    else
+      flash[:alert] = 'Record could not be updated!'
+    end
+
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def current_user_params
+    params.require(:user).permit(:about, :from, :status, :language)
+  end
+end
