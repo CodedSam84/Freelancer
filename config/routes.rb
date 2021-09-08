6314cedca4e5b98da7ff2d2aee4b77019e475b4c
Samuel Atobatele <swumatob@yahoo.co.uk>
2021-09-08 22:49:46 +0100

    Built UI for dashboard page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get '/dashboard', to: 'users#dashboard'
+  post '/users/edit', to: 'users#update'
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: 'profile' },
   controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
   root to: "pages#home"
