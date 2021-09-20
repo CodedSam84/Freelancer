73d1a741b8795e60060b8916adf98abaa152b2c9
Samuel Atobatele <swumatob@yahoo.co.uk>
2021-09-20 17:04:35 +0100

    Implemented file upload for user using active storage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,6 @@ class UsersController < ApplicationController
   private
 
   def current_user_params
-    params.require(:user).permit(:about, :from, :status, :language)
+    params.require(:user).permit(:about, :from, :status, :language, :avatar)
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :omniauthable, omniauth_providers: [:google_oauth2]
+  
+  has_one_attached :avatar
 
   validates :full_name, presence: true
   validates :full_name, length: { maximum: 20 }
