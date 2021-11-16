3f7f9ae82743daebe09ee57c1f699083cd1fae52
Samuel Atobatele <swumatob@yahoo.co.uk>
2021-11-16 13:29:10 +0100

    Updated the user dashboard page to show gigs created by the user

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,8 @@ module ApplicationHelper
       ActionController::Base.helpers.asset_path('icon_default_avatar.png')
     end
   end
+
+  def gig_photo_url(gig)
+    gig.photos.attached? ? url_for(gig.photos[0]): "icon_default_image.png"
+  end
 end
