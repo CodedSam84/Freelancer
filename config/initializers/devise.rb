94e2f057e2a2db13dd9fedfc8866eaf9c89c4c91
Samuel Atobatele <swumatob@yahoo.co.uk>
2021-12-10 00:13:42 +0100

    Updated google oauth 2 callbacks for production and development

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -274,9 +274,15 @@ Devise.setup do |config|
   #config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
 
   if Rails.env.production?
-    config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :id), Rails.application.credentials.dig(:google, :secret), { :redirect_uri => "https://freelancedo.herokuapp.com/auth/google_oauth2/callback" }
+    config.omniauth :google_oauth2, 
+                    Rails.application.credentials.dig(:google, :id), 
+                    Rails.application.credentials.dig(:google, :secret), 
+                    { :redirect_uri => "https://freelancedo.herokuapp.com/auth/google_oauth2/callback" }
   elsif Rails.env.development?
-    config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :id), Rails.application.credentials.dig(:google, :secret), { :redirect_uri => "http://localhost:3000/auth/google_oauth2/callback" }
+    config.omniauth :google_oauth2, 
+                    Rails.application.credentials.dig(:google, :id), 
+                    Rails.application.credentials.dig(:google, :secret), 
+                    { :redirect_uri => "http://localhost:3000/auth/google_oauth2/callback" }
   end
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
