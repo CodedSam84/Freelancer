e7534a33870cc57e10d98efd01f96a9b4acc0b99
Samuel Atobatele <swumatob@yahoo.co.uk>
2021-12-10 00:02:38 +0100

    fixed bug with google oauth 2

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -272,8 +272,12 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   #config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :id), Rails.application.credentials.dig(:google, :secret), { :redirect_uri => "https://freelancedo.herokuapp.com/auth/google_oauth2/callback" }
 
+  if Rails.env.production?
+    config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :id), Rails.application.credentials.dig(:google, :secret), { :redirect_uri => "https://freelancedo.herokuapp.com/auth/google_oauth2/callback" }
+  elsif Rails.env.development?
+    config.omniauth :google_oauth2, Rails.application.credentials.dig(:google, :id), Rails.application.credentials.dig(:google, :secret), { :redirect_uri => "http://localhost:3000/auth/google_oauth2/callback" }
+  end
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
   # change the failure app, you can configure them inside the config.warden block.
